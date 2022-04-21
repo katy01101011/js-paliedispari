b11f4f9ad439c4a08e5eb1d08f0e358e0305f5c3
katy01101011 <katy.91560@gmail.com>
2022-04-21 21:41:03 +0200

    add: "palindroma" function

--- Palindroma/js/script.js
+++ Palindroma/js/script.js
@@ -1,31 +1,29 @@
 // Chiedere all’utente di inserire una parola
-// const userWord = prompt('Inserisci una parola e io controllerò se è palindroma :)');
-// console.log(userWord);
-
-const userWord = 'ciao';
-console.log(userWord);
+const aWord = prompt('Digita una parola e ti dirò se è palindroma :)');
+console.log(aWord);
 
 // Creare una funzione per capire se la parola inserita è palindroma
-// function isPalindrome(wordToCheck) {
-//     wordToCheck = userWord;
-// }
+function isWordPalindrome(userWord) {
+    userWord = aWord.toLowerCase();
+    let j = Math.ceil(userWord.length / 2);
 
-// Creo un array vuoto che conterrà le lettere della parola da controllare
-let i = 0;
-let wordLetter;
-let sameLetter;
-let j = Math.ceil(userWord.length / 2);
+    let isPalindrome = true;
+    for (let i = 0; i < j; i++) {
+        let firstCheckLetter = userWord.charAt(i);
+        let secondCheckLetter = userWord.charAt(userWord.length - (i + 1));
 
-
-for (i = 0; i < j; i++) {
-    let firstCheckLetter = userWord.charAt(i);
-    let secondCheckLetter = userWord.charAt(userWord.length - (i + 1));
-
-    if (firstCheckLetter === secondCheckLetter) {
-        sameLetter = true;
-    } else {
-        sameLetter = false;
+        if (firstCheckLetter !== secondCheckLetter) {
+            isPalindrome = false;
+        }
     }
+    console.log(isPalindrome);
+    return isPalindrome;
 }
 
-console.log(sameLetter);
+// Controllare con la funzione se la parola inserita è palindroma o no
+let palindromeOrNot = isWordPalindrome(aWord);
+if (palindromeOrNot === true) {
+    document.getElementById('palindroma').innerHTML = 'La parola che hai inserito è palindroma';
+} else {
+    document.getElementById('palindroma').innerHTML = 'Ciò che hai digitato è una semplice, triste e comune parola :(';
+}
