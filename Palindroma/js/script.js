4578680a4093a0b554b6cd9914835b3132b13e23
katy01101011 <katy.91560@gmail.com>
2022-04-21 15:26:08 +0200

    add: palindroma

--- Palindroma/js/script.js
+++ Palindroma/js/script.js
@@ -1,1 +1,31 @@
-alert('ciao')
+// Chiedere all’utente di inserire una parola
+// const userWord = prompt('Inserisci una parola e io controllerò se è palindroma :)');
+// console.log(userWord);
+
+const userWord = 'ciao';
+console.log(userWord);
+
+// Creare una funzione per capire se la parola inserita è palindroma
+// function isPalindrome(wordToCheck) {
+//     wordToCheck = userWord;
+// }
+
+// Creo un array vuoto che conterrà le lettere della parola da controllare
+let i = 0;
+let wordLetter;
+let sameLetter;
+let j = Math.ceil(userWord.length / 2);
+
+
+for (i = 0; i < j; i++) {
+    let firstCheckLetter = userWord.charAt(i);
+    let secondCheckLetter = userWord.charAt(userWord.length - (i + 1));
+
+    if (firstCheckLetter === secondCheckLetter) {
+        sameLetter = true;
+    } else {
+        sameLetter = false;
+    }
+}
+
+console.log(sameLetter);
